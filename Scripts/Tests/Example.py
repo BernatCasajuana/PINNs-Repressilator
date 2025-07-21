a6dfc751247e989a0d35b7f875e1d8ea97ffd7fa
Bernat Casajuana <casajuana.bernat@mail.com>
2025-07-21 12:19:16 +0200

    changes

--- Scripts/Tests/Example.py
+++ Scripts/Tests/Example.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 import random
 
 # %% Define the neural network
-# 1 input layer, 3 hidden layers with 32 nodes each, tanh activation function, and 1 output layer.
+# 1 input layer, 3 hidden layers with 32 nodes each, tanh activation function, and 1 output layer
 NN = tf.keras.models.Sequential([
     tf.keras.layers.Input((1,)),
     tf.keras.layers.Dense(units = 32, activation = 'tanh'),
@@ -20,7 +20,7 @@ NN = tf.keras.models.Sequential([
 NN.summary()
 
 # %% Select optimizer
-# Adam optimizer with a learning rate of 0.001 (minimizing error between the predicted function and actual ODE solution).
+# Adam optimizer with a learning rate of 0.001 (minimizing error between the predicted function and actual ODE solution)
 optm = tf.keras.optimizers.Adam(learning_rate = 0.001)
 
 # %% Define ODE System and Loss Function
@@ -45,11 +45,11 @@ def ode_system(t, net):
     return total_loss
 
 # %% Training
-# Training data, time points.
+# Training data, time points
 train_t = (np.array([0., 0.25, 0.475, 0.5, 0.525, 0.6, 0.9, 0.95, 1., 1.05, 1.1, 1.25, 1.4, 1.45, 1.5, 1.55, 1.6, 1.75, 1.95, 2.])).reshape(-1, 1)
 train_loss_record = []
 
-# Training loop: 6000 iterations, loss calculation to update the weights of the NN.
+# Training loop: 6000 iterations, loss calculation to update the weights of the NN
 for itr in range(6000):
     with tf.GradientTape() as tape:
         train_loss = ode_system(train_t, NN)
@@ -62,7 +62,7 @@ for itr in range(6000):
         print(train_loss.numpy())
 
 # %% Plot Loss
-# Plotting the resulting loss over iterations.
+# Plotting the resulting loss over iterations
 plt.figure(figsize = (10,8))
 plt.plot(train_loss_record)
 plt.xlabel("Iteration")        
