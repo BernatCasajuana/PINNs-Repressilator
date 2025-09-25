12941a70293d30637c5870b1824075b4431abbe0
Bernat Casajuana <casajuana.bernat@mail.com>
2025-09-25 11:24:15 +0200

    jobs

--- jobs/forward_job.sh
+++ jobs/forward_job.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=forward_PINN         # Nom del job
 #SBATCH --output=forward_output.txt     # Fitxer de sortida
 #SBATCH --error=forward_error.txt       # Fitxer d’errors
-#SBATCH --time=04:00:00                 # Temps màxim (hh:mm:ss), ajustar segons necessitat
+#SBATCH --time=04:00:00                 # Temps màxim (hh:mm:ss)
 #SBATCH --cpus-per-task=4               # Nombre de CPUs per tasca
-#SBATCH --mem=8GB                        # Memòria assignada
+#SBATCH --mem=8GB                       # Memòria assignada
 
-# Entrar al directori del projecte (ajusta la ruta si cal)
+# Working directory
 cd $HOME/PINNs_Repressilator
 
-# Executar el script
+# Execute the Python script
 python scripts/run_all_forward.py
--- jobs/inverse_job.sh
+++ jobs/inverse_job.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=inverse_PINN         # Nom del job
 #SBATCH --output=inverse_output.txt     # Fitxer de sortida
 #SBATCH --error=inverse_error.txt       # Fitxer d’errors
-#SBATCH --time=04:00:00                 # Temps màxim (hh:mm:ss), ajustar segons necessitat
+#SBATCH --time=04:00:00                 # Temps màxim (hh:mm:ss)
 #SBATCH --cpus-per-task=4               # Nombre de CPUs per tasca
-#SBATCH --mem=8GB                        # Memòria assignada
+#SBATCH --mem=8GB                       # Memòria assignada
 
-# Entrar al directori del projecte (ajusta la ruta si cal)
+# Working directory
 cd $HOME/PINNs_Repressilator
 
-# Executar el script
+# Execute the Python script
 python scripts/run_all_inverse.py
